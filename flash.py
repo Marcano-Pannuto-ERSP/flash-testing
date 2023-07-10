ba268f74c0c234e8c69f13568ad497ee16d3bc22
Melody Gill <melodywgill@gmail.com>
2023-07-10 11:59:39 -0700

    Remove irrelevant functions

--- flash.py
+++ flash.py
@@ -17,21 +17,6 @@ class Flash:
         self.spi.deinit()
         self.cs.init(Pin.IN)
 
-    def read_byte(self, command):
-        self.cs.value(0)
-        self.spi.write(bytes([command]))
-        data = self.spi.read(1)[0]
-        self.cs.value(1)
-        return data
-
-    def write_byte(self, command, data=None):
-        self.cs.value(0)
-        if data == None:
-            self.spi.write(bytes([command]))
-        else:
-            self.spi.write(bytes([command, data]))
-        self.cs.value(1)
-
     # Write the command then read size bytes
     def read_bulk(self, command, size):
         self.cs.value(0)
@@ -41,17 +26,10 @@ class Flash:
         return data
 
     # Written for 0x03 commmand
-    def read_data(self, size, addr):
+    def read_data(self, addr, size):
         self.cs.value(0)
         toWrite = [addr >> 16 & 0xFF, addr >> 8 & 0xFF, addr & 0xFF]
         self.spi.write(bytes([0x03] + toWrite))
         data = self.spi.read(size)
         self.cs.value(1)
         return data
-
-    # not sure abt this one
-    # probably do the same thing with data=None
-    def write_bulk(self, command, data):
-        self.cs.value(0)
-        self.spi.write(bytes([command]) + data)
-        self.cs.value(1)
